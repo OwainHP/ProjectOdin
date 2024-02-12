6f0252ef9da19c6fa5cbfd705bbca1a75edf98a5
OwainHP <157285320+OwainHP@users.noreply.github.com>
2024-02-12 00:43:22 +0000

    Create Calculator

--- Projects/3Calculator/javascript.js
+++ Projects/3Calculator/javascript.js
@@ -1,1 +1,44 @@
-function
+const btn = document.querySelectorAll(".number");
+const display = document.querySelector(".display");
+const equals = document.querySelector(".sum")
+const clear = document.querySelector(".cancel")
+const operator = document.querySelectorAll(".operator")
+
+  btn.forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (display.innerText=="0") {
+            display.innerText=""
+        };
+        let text = button.innerText;
+        display.innerText = display.innerText+text;
+    });
+  });
+
+
+
+operator.forEach(function(operator) {
+    operator.addEventListener("click", function() {
+        number = display.innerText;
+        display.innerText = "0";
+        operatorToUse = operator.innerText;
+    });
+  });
+
+equals.addEventListener("click", function() {
+    number2 = display.innerText;
+    if (operatorToUse == "+") {
+        display.innerText = Number(number)+Number(number2);
+    } else if (operatorToUse == "-") {
+        display.innerText = Number(number)-Number(number2);
+    } else if (operatorToUse == "÷") {
+        display.innerText = Number(number)/Number(number2);
+    } else if (operatorToUse == "x") {
+        display.innerText = Number(number)*Number(number2);
+    }
+})
+
+clear.addEventListener("click", function() {
+    display.innerText = "0";
+    number = 0;
+    number2 = 0;
+})
